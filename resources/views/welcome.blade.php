3111ac258eaf9457bb59a38623f1516a69103e7e
OgataiKhan <badellino.kevin@gmail.com>
2024-01-29 17:53:29 +0100

    feat: card hover effect & favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Model Controller</title>
+    {{-- Favicon --}}
+    <link rel="icon" href="{{ asset('boolflix-favicon.webp') }}">
+    {{-- /Favicon --}}
     {{-- JS & CSS --}}
     @vite('resources/js/app.js')
     {{-- /JS & CSS --}}
