330c545387ea5753189cfdaafce070887576e7d9
Kevin Luca Bochenski Badellino <67221885+OgataiKhan@users.noreply.github.com>
2024-01-29 15:38:50 +0100

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+// CSS import
+import '~resources/scss/app.scss';
+
+// Boostrap JS
+import * as bootstrap from 'bootstrap';
+
+// Process images
+import.meta.glob([
+  '../img/**'
+]);
