ba7e4424a97bc3e90b8f9f4b11b6784ee5985898
OgataiKhan <badellino.kevin@gmail.com>
2024-01-29 17:03:32 +0100

    feat: display data

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all(); // Fetch movies from database
+        return view('welcome', ['movies' => $movies]);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,5 +11,10 @@
 </head>
 <body>
     <h1>Ciao</h1>
+    <ul>
+        @foreach ($movies as $movie)
+            <li>{{ $movie->title }} - {{ $movie->original_title }}</li>
+        @endforeach
+    </ul>
 </body>
 </html>
